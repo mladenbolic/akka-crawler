ec24334ff7b6ad61145a6531901f1cd712fc205e
Mladen Bolic <mladen.bolic@sixhours.io>
2018-10-05 18:02:58 +0200

    Remove unused test

--- src/test/java/AppTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.Test;
-
-public class AppTest {
-    @Test public void testAppHasAGreeting() {
-//        App classUnderTest = new App();
-//        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
-}
