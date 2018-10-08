e924905ff9a301b5f7fc3f9f089c3c5f1df73e99
Mladen Bolic <mladen.bolic@sixhours.io>
2018-10-08 17:29:05 +0200

    Update file download actor test

--- src/test/java/io/sixhours/crawler/downloader/FileDownloadActorTest.java
+++ src/test/java/io/sixhours/crawler/downloader/FileDownloadActorTest.java
@@ -22,18 +22,21 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class FileDownloadActorTest {
 
+  private static final String TEST_URL = "http://test.url";
+  private static final String TEST_PATH = "/test/path";
+
   static ActorSystem system;
 
   @Mock
   private FileDownloader fileDownloader;
 
   @BeforeClass
-  public static void setup() {
+  public static void setUpClass() {
     system = ActorSystem.create();
   }
 
   @AfterClass
-  public static void teardown() {
+  public static void tearDownClass() {
     TestKit.shutdownActorSystem(system);
     system = null;
   }
@@ -41,35 +44,35 @@ public class FileDownloadActorTest {
   @Test
   public void givenUrl_whenFileDownload_thenReturnDownloadResult() throws Exception {
     TestKit probe = new TestKit(system);
-    when(fileDownloader.downloadFile(any(String.class), any(String.class)))
-        .thenReturn(new FileDownloadResult("http://some.url", "/some/path"));
+    when(fileDownloader.downloadFile(any(String.class)))
+        .thenReturn(new FileDownloadResult(TEST_URL, TEST_PATH));
 
-    ActorRef fileDownloaderActor = system.actorOf(FileDownloadActor.props("group", fileDownloader));
+    ActorRef fileDownloaderActor = system.actorOf(FileDownloadActor.props(fileDownloader));
 
-    fileDownloaderActor.tell(new DownloadFile("http://some.url"), probe.getRef());
+    fileDownloaderActor.tell(new DownloadFile(TEST_URL), probe.getRef());
 
     FileDownloadResult response = probe.expectMsgClass(FileDownloadResult.class);
 
     verify(fileDownloader, times(1))
-        .downloadFile(any(String.class), any(String.class));
-    assertThat(response.getUrl()).isEqualTo("http://some.url");
-    assertThat(response.getPath()).isEqualTo("/some/path");
+        .downloadFile(any(String.class));
+    assertThat(response.getUrl()).isEqualTo(TEST_URL);
+    assertThat(response.getPath()).isEqualTo(TEST_PATH);
   }
 
   @Test
   public void givenUrl_whenFileDownloadException_thenReturnErrorResult() throws Exception {
     TestKit probe = new TestKit(system);
-    when(fileDownloader.downloadFile(any(String.class), any(String.class)))
+    when(fileDownloader.downloadFile(any(String.class)))
         .thenThrow(new FileDownloadException("error"));
 
-    ActorRef fileDownloaderActor = system.actorOf(FileDownloadActor.props("group", fileDownloader));
+    ActorRef fileDownloaderActor = system.actorOf(FileDownloadActor.props(fileDownloader));
 
-    fileDownloaderActor.tell(new DownloadFile("http://some.url"), probe.getRef());
+    fileDownloaderActor.tell(new DownloadFile(TEST_URL), probe.getRef());
 
     FileDownloadError response = probe.expectMsgClass(FileDownloadError.class);
 
     verify(fileDownloader, times(1))
-        .downloadFile(any(String.class), any(String.class));
-    assertThat(response.getUrl()).isEqualTo("http://some.url");
+        .downloadFile(any(String.class));
+    assertThat(response.getUrl()).isEqualTo(TEST_URL);
   }
 }
